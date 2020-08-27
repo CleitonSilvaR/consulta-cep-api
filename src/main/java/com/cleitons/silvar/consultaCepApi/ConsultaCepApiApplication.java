18b6366e421ae8a0ab5683c16367215180113fad
Cleiton Silva <cleiton_.98@hotmail.com>
2020-08-26 23:19:06 -0300

    base do projeto

---
+++ src/main/java/com/cleitons/silvar/consultaCepApi/ConsultaCepApiApplication.java
@@ -0,0 +1,13 @@
+package com.cleitons.silvar.consultaCepApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaCepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaCepApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleitons/silvar/consultaCepApi/ConsultaCepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleitons.silvar.consultaCepApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
